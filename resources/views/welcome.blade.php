a88b3935d0be4dceef4c12d3227c03e51859f076
cesarahernandez <cesarahernandez127@gmail.com>
2019-04-07 20:16:13 -0700

    Set up laradock

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Time Classics</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Time Classic Movies Baby
                 </div>
 
                 <div class="links">
